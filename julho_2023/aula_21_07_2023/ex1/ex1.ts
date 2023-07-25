835a262fee34d6e3779d4eb0f54bd5cce8f7aa68
Rafael Custodio Silva <rafaelcustodio@msn.com>
2023-07-25 09:37:17 -0300

    Exercício TypeScript
    Exercício TypeScript

---
+++ julho_2023/aula_21_07_2023/ex1/ex1.ts
@@ -0,0 +1,111 @@
+// Instalação TypeScript -> npm i -g typescript
+// Instalação de pacote para rodar no terminal -> npm i -g ts-node
+// Instalação de Prompt-Sync ->npm i prompt-sync
+// Rodar prompt-sync -> npm i --save-dev @types/prompt-sync
+// Para rodar no terminal-> npx ts-node nome_do_arquivo.ts
+
+
+/*
+Fazer um menu de exercícios utilizando exemplos dado em aula 
+(6 exercícios dentro de funções, e incluir nome ou algo de sua 
+    preferência para criar um objeto e manipulá-lo)
+*/
+
+// Modo ANTIGO de receber informações do usuário
+// let prompt = require("prompt-sync")()
+
+//Modo NOVO de receber informações do usuário
+import promptSync from "prompt-sync"
+import { exercicio1, exercicio2, exercicio3, exercicio4, exercicio5, exercicio6, exercicio7, exercicio8, exercicio9, exercicio10 } from "./moduloEx1";
+
+let prompt = promptSync()
+let resposta: string
+
+do {
+    console.log(" ")
+    console.log("======================================================")
+    console.log(" ")
+    console.log("Bem vindo ao menu de exercícios!")
+    resposta = prompt("Digite um número de 1 a 10 para escolher um exercício, ou 'sair' para sair: ")
+
+    switch (resposta) {
+        case "1":
+            console.log("Você escolheu o exercício 1.")
+
+            //Chamar (invocar) o exercício 1
+            exercicio1()
+            break
+
+        case "2":
+            console.log("Você escolheu o exercício 2.")
+
+            //Chamar (invocar) o exercício 2
+            exercicio2()
+            break
+
+        case "3":
+            console.log("Você escolheu o exercício 3.")
+
+            //Chamar (invocar) o exercício 3
+            exercicio3()
+            break
+
+        case "4":
+            console.log("Você escolheu o exercício 4.")
+
+            //Chamar (invocar) o exercício 4
+            exercicio4()
+            break
+
+        case "5":
+            console.log("Você escolheu o exercício 5.")
+
+            //Chamar (invocar) o exercício 5
+            exercicio5()
+            break
+
+        case "6":
+            console.log("Você escolheu o exercício 6.")
+
+            //Chamar (invocar) o exercício 6
+            exercicio6()
+            break
+
+        case "7":
+            console.log("Você escolheu o exercício 7.")
+
+            //Chamar (invocar) o exercício 7
+            exercicio7()
+            break
+
+        case "8":
+            console.log("Você escolheu o exercício 8.")
+
+            //Chamar (invocar) o exercício 8
+            exercicio8()
+            break
+
+        case "9":
+            console.log("Você escolheu o exercício 9.")
+
+            //Chamar (invocar) o exercício 9
+            exercicio9()
+            break
+
+        case "10":
+            console.log("Você escolheu o exercício 10.")
+
+            //Chamar (invocar) o exercício 10
+            exercicio10()
+            break
+
+        default:
+            console.log("Você não escolheu nenhum exercício.")
+            break
+    }
+
+} while (resposta != "sair")
+
+console.log(" ")
+console.log("======================================================")
+console.log(" ")
---
+++ julho_2023/aula_21_07_2023/ex1/moduloEx1.ts
@@ -0,0 +1,210 @@
+// Instalação TypeScript -> npm i -g typescript
+// Instalação de pacote para rodar no terminal -> npm i -g ts-node
+// Instalação de Prompt-Sync ->npm i prompt-sync
+// Rodar prompt-sync -> npm i --save-dev @types/prompt-sync
+// Para rodar no terminal-> npx ts-node nome_do_arquivo.ts
+
+
+
+import promptSync from "prompt-sync"
+let prompt = promptSync()
+
+
+export function exercicio1() {
+
+    console.log("Exercício 1")
+    console.log("Exercício com FOR = Imprimir números de 1 a 10 utilizando o loop.");
+
+    for (let contador: number = 1; contador < 11; contador++) {
+        console.log(contador)
+    }
+}
+
+
+
+
+export function exercicio2() {
+
+    console.log("Exercício 2");
+
+    console.log("Exercício com FOR = Imprimir números pares de 1 a 10.");
+    //for(let contador = 0; contador < 11; contador = contador+2){
+    for (let contador: number = 0; contador <= 10; contador += 2) {
+        console.log(contador)
+    }
+}
+
+
+
+
+export function exercicio3() {
+
+    console.log("Exercício com FOR = Faça um programa que imprima os números ímpares de 0 a 50.");
+
+    //for(let contador = 1; contador < 50; contador = contador+2){
+    for (let contador: number = 1; contador < 50; contador += 2) {
+        console.log(contador)
+    }
+}
+
+
+
+
+export function exercicio4() {
+
+    console.log("Exercício com FOR = Faça um programa que imprima na tela a tabuada de um número qualquer escolhido pelo usuário até o 10.");
+
+    let numero: number = parseInt(prompt("Digite um número inteiro de 0 a 10: "))
+
+    console.log("========================================")
+    console.log(" ")
+    console.log("TABUADA DO " + numero)
+    console.log(" ")
+
+    for (let i: number = 0; i <= 10; i * numero) {
+        console.log(`${numero} x ${i}  = ` + (i * numero))
+        i += 1
+    }
+}
+
+
+
+
+export function exercicio5() {
+
+    console.log("Exercício com FOR = Imprimir os primeiros 10 números da sequência de Fibonacci.");
+
+    let numero1: number = 0
+    let numero2: number = 1
+    let soma: number = 0
+
+    console.log(numero1)
+    console.log(numero2)
+
+    for (let i: number = 1; i <= 8; i++) {
+        soma = numero1 + numero2
+        console.log(soma)
+        numero1 = numero2
+        numero2 = soma
+    }
+}
+
+
+
+
+export function exercicio6() {
+
+    console.log("Exercício com WHILE = Imprimir números pares de 0 a 20 utilizando o loop.")
+
+    let numero: number = 0
+
+    while (numero <= 20) {
+        console.log(numero)
+        numero = numero + 2
+    }
+}
+
+
+export function exercicio7() {
+
+    console.log("Exercício com WHILE = Calcular a soma dos números de 1 a 100 utilizando a estrutura.");
+
+    let contador: number = 1
+    let soma: number = 0
+    console.log()
+
+    while (contador <= 100) {
+        soma = soma + contador
+        //soma += contador    
+
+        console.log(`Passando ${contador} vez, a soma é ${soma}.`)
+        //console.log(contador)
+
+        contador++
+    }
+}
+
+
+
+export function exercicio8() {
+
+    console.log("Exercício com WHILE = Faça um programa que receba a idade do usuário, enquanto a resposta do usuário for menor que 18 anos continue a solicitar a idade e mostre uma mensagem quando a idade do usuário for maior que 18.");
+
+    console.log("========================================")
+    console.log(" ")
+
+    let idade: number = parseInt(prompt("Digite a sua idade: "))
+
+    while (idade < 18) {
+        idade = parseInt(prompt("Digite a sua idade: "))
+    }
+    console.log("Você tem 18 anos ou mais.")
+    console.log(" ")
+    console.log("========================================")
+
+}
+
+
+
+
+export function exercicio9() {
+
+    console.log("Exercício com WHILE = Faça um programa que receba um número do usuário e mostre no console a contagem regressiva até 0.");
+
+    let numero: number = parseInt(prompt("Digite um número de 1 a 50: "))
+
+    console.log("========================================")
+    console.log(" ")
+    while (numero >= 0) {
+        console.log(numero)
+        numero--
+    }
+    console.log(" ")
+    console.log("Saiu do loop")
+
+    console.log(" ")
+    console.log("========================================")
+}
+
+
+
+
+export function exercicio10() {
+
+    console.log("Exercício com WHILE = Faça um programa que receba um número do usuário e continue a pedir o número se o numero for diferente de 0.");
+
+    let numero: number = 0
+
+    console.log("========================================")
+    console.log(" ")
+
+    numero = parseInt(prompt("Digite um número qualquer: "))
+
+    while (numero != 0) {
+        numero = parseInt(prompt("Digite um número qualquer: "))
+    }
+    console.log(" ")
+    console.log("Digitado o 0")
+    console.log("Saiu do loop")
+
+    console.log(" ")
+    console.log("========================================")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
